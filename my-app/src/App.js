0fb7b3715c5c1e591ef58b286e22112a018e078d
Marcin <kwiatekm4@gmail.com>
2021-06-04 15:48:52 +0200

    adding bootstrap and start page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,58 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import {Button, Container, Row, Col, Form} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+        <Container style={{width:'30%'}}>
+          <Row  style={{marginBottom:20, width:'100%'}}>
+            <Col><Form.Text style={{color:'black'}} >Liczba miejsc:</Form.Text></Col>
+            <Col><Form.Control type="number" min="1"></Form.Control></Col>
+          </Row>
+          <Form.Check className="placesTogether" type="checkbox" label="Czy miejsca mają być obok siebie?"></Form.Check>
+        <Button className='placeSelectorButton'>Wybierz miejsca</Button>
+        </Container>
       </header>
     </div>
   );
 }
-
 export default App;
